2c6b179c899d5d1fd65f2874ad2ba94045f29e6d
Man 150 <man.davra@swiftrut.com>
2025-01-08 01:50:17 +0530

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
-// Before: CommonJS Syntax
-// const colors = require('tailwindcss/colors');
-
-// After: ESM Syntax
-import colors from 'tailwindcss/colors.js';
-
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{html,js,jsx}',
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      colors: {
-        teal: colors.teal,
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
