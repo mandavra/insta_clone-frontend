46698428b38cd666815266fd3c947b977bed03c2
Man 150 <man.davra@swiftrut.com>
2025-01-08 01:44:50 +0530

    chate sumchanges in frontend side

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// Before: CommonJS Syntax
+// const colors = require('tailwindcss/colors');
+
+// After: ESM Syntax
+import colors from 'tailwindcss/colors.js';
+
 export default {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{html,js,jsx}',
-  ],
-  prefix: "",
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+      colors: {
+        teal: colors.teal,
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
