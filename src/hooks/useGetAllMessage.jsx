5a830344b8cc82da628eaa1a53a7020843f58829
Man 150 <man.davra@swiftrut.com>
2025-01-11 05:41:29 +0530

    fix: correct API endpoint URLs in multiple components

--- src/hooks/useGetAllMessage.jsx
+++ src/hooks/useGetAllMessage.jsx
@@ -10,7 +10,7 @@ const useGetAllMessage = () => {
     useEffect(() => {
         const fetchAllMessage = async () => {
             try {
-                const res = await axios.get(`https://insta-clone-1-fqbz.onrender.comapi/v1/message/all/${selectedUser?._id}`, { withCredentials: true });
+                const res = await axios.get(`https://insta-clone-1-fqbz.onrender.com/api/v1/message/all/${selectedUser?._id}`, { withCredentials: true });
                 if (res.data.success) {  
                     dispatch(setMessages(res.data.messages));
                 }
